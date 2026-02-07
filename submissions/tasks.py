35195bc6b33306e84ae6cf9a47ad145c8a84f601
NavadeepBoyana <boyananavadeep@gmail.com>
2026-02-07 21:55:07 +0530

    Fix Bash image tag

--- submissions/tasks.py
+++ submissions/tasks.py
@@ -32,7 +32,7 @@ LANGUAGE_CONFIG = {
         'run_cmd': 'node /code/{filename}'
     },
     'bash': {
-        'image': 'bash:5',
+        'image': 'devel-alphine3.23',
         'file_name': 'script.sh',
         'run_cmd': 'bash /code/script.sh'
     },
