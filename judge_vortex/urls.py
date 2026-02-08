d4f380897335704f7c07062ecd976b6f9d24fec1
NavadeepBoyana <boyananavadeep@gmail.com>
2026-02-08 23:12:46 +0530

    Bugs resolved

--- judge_vortex/urls.py
+++ judge_vortex/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('mysiteadministration/admin/interface', admin.site.urls),
     path('', include('submissions.urls')),
 ]
