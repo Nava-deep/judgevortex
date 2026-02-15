e719c8e1774d1e5e46266437767e964eedc4ff67
NavadeepBoyana <boyananavadeep@gmail.com>
2026-02-15 12:55:48 +0530

    Add new changes

--- judge_vortex/urls.py
+++ judge_vortex/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('mysiteadministration/admin/interface', admin.site.urls),
     path('', include('submissions.urls')),
+    path('', include('django_prometheus.urls')),
 ]
